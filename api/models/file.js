ac6e4f69a55e588525ee6f1502bb8816a534eac1
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2019-08-25 12:36:50 +0400

    Combine folder model into file model
    Differentiating each unnecessarily complicates the API, and functionality of both is similar

--- api/models/file.js
+++ api/models/file.js
@@ -1,23 +1,19 @@
+//Directory is a file, too
 const mongoose = require('mongoose');
 
 const fileSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {
-        type: String,
-        required: true
-    },
-    mimetype: {
-        type: String,
-        required: true
-    },
-    size: {
-        type: Number,
-        required: true
-    },
+    name: String,
+    mimetype: String,
+    size: Number,
     parent: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Folder'
+        ref: 'File'
     },
+    children: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    }],
     path: {
         type: String,
         required: true
