4db0b559634b7f08b078eeed4352286a7f5cdd13
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2019-08-25 12:49:18 +0400

    Improved code

--- api/models/file.js
+++ api/models/file.js
@@ -1,18 +1,38 @@
 //Directory is a file, too
 const mongoose = require('mongoose');
 
+const fileOptional = () => { return this.type === 'file' };
+const directoryOptional = () => { return this.type === 'directory' };
+
+//https://stackoverflow.com/a/40714371
 const fileSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    mimetype: String,
-    size: Number,
+    type: {
+        type: String,
+        enum: ['file', 'directory'],
+        required: true
+    },
+    name: {
+        type: String,
+        required: fileOptional
+    },
+    mimetype: {
+        type: String,
+        required: fileOptional
+    },
+    size: {
+        type: String,
+        required: fileOptional
+    },
     parent: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'File'
+        ref: 'File',
+        required: true
     },
     children: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'File'
+        ref: 'File',
+        required: directoryOptional
     }],
     path: {
         type: String,
